f09c3ae186e2045e5af82547b3c73748844bf8f1
nexusameer <ameerk10fw@gmail.com>
2025-04-29 20:47:08 +0500

    commit

--- Assignment/modules/storage_account/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "storage_account_name" {}
-variable "rg_name" {}
-variable "location" {}
--- Assignment/modules/vnet/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "vnet_name" {}
-variable "rg_name" {}
-variable "location" {}
-variable "subnet_name" {}
-variable "address_space" {}
-variable "subnet_prefix" {}
