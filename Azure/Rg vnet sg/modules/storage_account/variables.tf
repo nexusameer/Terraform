0e3de774467c996ac14c7fee3e8a83a316520f18
nexusameer <ameerk10fw@gmail.com>
2025-07-01 17:18:56 +0500

    fix error

---
+++ Azure/Rg vnet sg/modules/storage_account/variables.tf
@@ -0,0 +1,15 @@
+variable "storage_name"     { 
+    type = string 
+}
+variable "rg_name"          { 
+    type = string 
+}
+variable "location"         { 
+    type = string 
+}
+variable "account_tier"     { 
+    type = string 
+}
+variable "replication_type" { 
+    type = string 
+}
